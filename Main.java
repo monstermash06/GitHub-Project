02b740d686dbf46c7f35596775910df913097e7e
monstermash06 <73838252+monstermash06@users.noreply.github.com>
2025-04-24 14:18:57 -0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,137 +1,87 @@
-import java.net.*;
-import javafx.application.*;
-import javafx.scene.*;
-import javafx.scene.text.*;
-import javafx.stage.*;
-import javafx.scene.control.*;
-import javafx.scene.layout.*;
-import java.util.*;
-import javafx.scene.paint.*;
-import javafx.geometry.*;
-import javafx.scene.image.*;
+import javafx.animation.AnimationTimer;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.image.Image;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.stage.Stage;
 
-import java.io.*;
+public class Main extends Application {
 
-import java.util.*;
-import java.text.*;
-import java.io.*;
-import java.lang.*;
-import javafx.application.*;
-import javafx.event.*;
-import javafx.stage.*;
-import javafx.scene.canvas.*;
-import javafx.scene.paint.*;
-import javafx.scene.*;
-import javafx.scene.input.*;
-import javafx.scene.layout.*;
-import javafx.animation.*;
-import javafx.scene.control.*;
-import javafx.scene.image.*;
-import java.net.*;
-import javafx.geometry.*;
+    private Player thePlayer;
+    private Canvas canvas;
+    private Image background;
+    private double highScore = 0;
 
+    public void start(Stage stage) {
+        thePlayer = new Player();
+        background = new Image("bg1.png");
 
-public class Main extends Application
-{
-   FlowPane fp;
-   
-   Canvas theCanvas = new Canvas(600,600);
+        Pane root = new Pane();
+        canvas = new Canvas(800, 600);
+        root.getChildren().add(canvas);
+        GraphicsContext gc = canvas.getGraphicsContext2D();
 
-   public void start(Stage stage)
-   {
-      
-   
-      fp = new FlowPane();
-      fp.getChildren().add(theCanvas);
-      gc = theCanvas.getGraphicsContext2D();
-      drawBackground(300,300,gc);
-      
-      
-      Scene scene = new Scene(fp, 600, 600);
-      stage.setScene(scene);
-      stage.setTitle("Project :)");
-      stage.show();
-      
-      
-   }
-   
-   GraphicsContext gc;
-   
-   
-   
-   Image background = new Image("stars.png");
-   Image overlay = new Image("starsoverlay.png");
-   Random backgroundRand = new Random();
-   //this piece of code doesn't need to be modified
-   public void drawBackground(float playerx, float playery, GraphicsContext gc)
-   {
-	  //re-scale player position to make the background move slower. 
-      playerx*=.1;
-      playery*=.1;
-   
-	//figuring out the tile's position.
-      float x = (playerx) / 400;
-      float y = (playery) / 400;
-      
-      int xi = (int) x;
-      int yi = (int) y;
-      
-	  //draw a certain amount of the tiled images
-      for(int i=xi-3;i<xi+3;i++)
-      {
-         for(int j=yi-3;j<yi+3;j++)
-         {
-            gc.drawImage(background,-playerx+i*400,-playery+j*400);
-         }
-      }
-      
-	  //below repeats with an overlay image
-      playerx*=2f;
-      playery*=2f;
-   
-      x = (playerx) / 400;
-      y = (playery) / 400;
-      
-      xi = (int) x;
-      yi = (int) y;
-      
-      for(int i=xi-3;i<xi+3;i++)
-      {
-         for(int j=yi-3;j<yi+3;j++)
-         {
-            gc.drawImage(overlay,-playerx+i*400,-playery+j*400);
-         }
-      }
-   }
-   
-   
-   
-   
-   
+        Scene scene = new Scene(root);
+        setupInput(scene);
 
-   
-   public class AnimationHandler extends AnimationTimer
-   {
-      public void handle(long currentTimeInNanoSeconds) 
-      {
-         gc.clearRect(0,0,600,600);
-         
-         //USE THIS CALL ONCE YOU HAVE A PLAYER
-         //drawBackground(thePlayer.getX(),thePlayer.getY(),gc); 
+        new AnimationTimer() {
+            public void handle(long now) {
+                thePlayer.act();
+                drawBackground(gc, thePlayer.getX(), thePlayer.getY());
+                thePlayer.drawMe(gc);
+                drawScore(gc);
+            }
+        }.start();
 
+        stage.setTitle("Mine Avoider");
+        stage.setScene(scene);
+        stage.show();
+    }
 
-	      //example calls of draw - this should be the player's call for draw
-         //thePlayer.draw(300,300,gc,true); //all other objects will use false in the parameter.
+    private void setupInput(Scene scene) {
+        scene.setOnKeyPressed(e -> {
+            switch (e.getCode()) {
+                case W -> thePlayer.setDirection("W", true);
+                case A -> thePlayer.setDirection("A", true);
+                case S -> thePlayer.setDirection("S", true);
+                case D -> thePlayer.setDirection("D", true);
+            }
+        });
 
-         //example call of a draw where m is a non-player object. Note that you are passing the player's position in and not m's position.
-         //m.draw(thePlayer.getX(),thePlayer.getY(),gc,false);
-         
-      }
-   }
+        scene.setOnKeyReleased(e -> {
+            switch (e.getCode()) {
+                case W -> thePlayer.setDirection("W", false);
+                case A -> thePlayer.setDirection("A", false);
+                case S -> thePlayer.setDirection("S", false);
+                case D -> thePlayer.setDirection("D", false);
+            }
+        });
+    }
 
-   public static void main(String[] args)
-   {
-      launch(args);
-   }
+    private void drawBackground(GraphicsContext gc, double x, double y) {
+        // Draw background relative to player position
+        double bgX = -(x % background.getWidth());
+        double bgY = -(y % background.getHeight());
+
+        for (int i = -1; i <= 1; i++) {
+            for (int j = -1; j <= 1; j++) {
+                gc.drawImage(background, bgX + i * background.getWidth(), bgY + j * background.getHeight());
+            }
+        }
+    }
+
+    private void drawScore(GraphicsContext gc) {
+        gc.setFill(Color.BLACK);
+        gc.setFont(new Font("Arial", 20));
+        gc.fillText("Score: " + String.format("%.2f", thePlayer.getScore()), 20, 30);
+        gc.fillText("High Score: " + String.format("%.2f", highScore), 20, 55);
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
-
